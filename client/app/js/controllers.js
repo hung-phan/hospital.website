136c0d43727886009cc14c024ab57f3529ee2232
colorvisa <colorvisavn@gmail.com>
2013-10-13 18:23:31 +0700

    Working on server-side part

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -13,10 +13,34 @@ define([
         'hospitalServices'
     ]).controller('Login', [
         '$scope',
-        '$route',
+        '$location',
         'LoginService', 
-        function($scope, $route, LoginService) {
+        function($scope, $location, LoginService) {
+            /* initialize */
+            $scope.signin = {
+                username : '',
+                password : '',
+                remember : false
+            };
 
+            /* login function */
+            $scope.login = function() {
+                var signin = new LoginService();
+                func.simpleExtend(signin, {
+                            username : $scope.signin.username,
+                            password : $scope.signin.password
+                        });
+                signin.$save({}, function(data) {
+                    if (data.validUser) {
+                        var role = data.role;
+                        // remember me
+                        $location.path('/home');
+                    }
+                }, function(data) {
+                    alertify.error('Server down, try again later'); 
+                });
+
+            }
         }
     ]).controller('CreateAccount', [
         '$scope', 
@@ -42,7 +66,6 @@ define([
             $scope.createAccount = function() {
                 /* validation */ 
                 $scope.signup.passwordIdentical = $scope.signup.password == $scope.signup.confirmation;
-                // uncomment later
                 // $scope.signup.usernameExist = CheckUsernameExistService.query().usernameExist;
                 $scope.signup.error = !$scope.signup.passwordIdentical || $scope.signup.usernameExist;
                 
@@ -50,16 +73,19 @@ define([
                     /* insert account into the server database */
                     var signup = new CreateAccountService();
                     func.simpleExtend(signup, {
-                        type : 'CreateAccount',
-                        firstName : $scope.signup.firstName,
-                        lastName : $scope.signup.lastName,
-                        username : $scope.signup.username,
-                        password : $scope.signup.password
+                                type : 'CreateAccount',
+                                firstName : $scope.signup.firstName,
+                                lastName : $scope.signup.lastName,
+                                username : $scope.signup.username,
+                                password : $scope.signup.password
+                            });
+                    signup.$save({}, function(data) {
+                        /* successfully create account */
+                        alertify.success('Account successfully created');
+                        $location.path('/login'); 
+                    }, function(data) {
+                        alertify.error('Server down, try again later');
                     });
-                    // uncomment later
-                    // signup.$save();
-                    alertify.log('Account successfully created');
-                    $location.path('/login'); 
                 }
             }
         }
